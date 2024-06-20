879a63028005e3f54ef4eacd98ff0516efd3f9a9
younousfofana <younousfofana18@gmail.com>
2024-06-20 13:50:57 +0200

    Update Products App ==> create views, create template(index.html, Update urls(ecommerce))

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,10 @@
+from urllib import request
 from django.shortcuts import render
+from apps.products.models import Products
 
-# Create your views here.
+def index(request):
+    products_list = Products.objects
+    context = {
+        "products_list" : products_list
+    }
+    return render(request,"products/index.html", context)
