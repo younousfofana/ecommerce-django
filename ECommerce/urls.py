d98ffb4ad24d47ec6e144fa1dfe4d7e4c2cfc044
younousfofana <younousfofana18@gmail.com>
2024-08-26 13:54:13 +0200

    Update base.html ==> update view for logout user and display login if user isn't connected, Update url.py(root) ==> specify route after deconnection

--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('cart/', include('apps.Cart.urls')),
     path('products/', include("apps.products.urls")),
     path('login/', auth_views.LoginView.as_view(), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name="logout"),
     path('', include("apps.products.urls")),
     path('admin/', admin.site.urls),
 ]
