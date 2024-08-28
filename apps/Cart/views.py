9651aae39eb8d74eb7faa6306c87e4b8747ebd7e
younousfofana <younousfofana18@gmail.com>
2024-08-28 18:08:05 +0200

    Update views(app\Cart), cart_summary.html, base.html

--- apps/Cart/views.py
+++ apps/Cart/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 from .models import Products, Cart, CartItems
 from django.http import JsonResponse
 
-""" @login_required """
 def add_to_cart(request, product_id):
     product = get_object_or_404(Products, pk=product_id)
     quantity = int(request.POST.get('quantity', 1))
-
-    print('quantite envoye depuis le formulaire', quantity);
 
     if request.user.is_authenticated:
         cart, created = Cart.objects.get_or_create(user=request.user)
@@ -21,26 +17,27 @@ def add_to_cart(request, product_id):
 
     cart_item, created = CartItems.objects.get_or_create(cart=cart, product=product)
 
-    # Mettre à jour la quantité avec la nouvelle valeur sans ajouter à l'existante
     if quantity <= product.quantity:
         cart_item.quantity = quantity
         cart_item.save()
 
     return JsonResponse({'status': 'Product added to cart successfully.'})
 
-
-""" @login_required """
 def cart_summary(request):
-    # Récupération du panier de l'utilisateur connecté ou du panier de session
+    cartItems = []
+
     if request.user.is_authenticated:
-        cart = Cart.objects.get(user=request.user)
+        cart = get_object_or_404(Cart, user=request.user)
+        cartItems = cart.cartitems_set.all()
     else:
         session_key = request.session.session_key
-        if not session_key:
-            cart = None
+        if session_key:
+            cart = Cart.objects.filter(session_key=session_key).first();
+            if cart:
+                cartItems = cart.cartitems_set.all();
         else:
-            cart = Cart.objects.get(session_key=session_key)
-            print('le panier de session', cart);
-        cartItems = cart.cartitems_set.all() if cart else []
+            request.session.create()
+            
+    total_products_in_card = len(cartItems);
 
-    return render(request, 'cart_summary.html', {'cart_items' : cartItems})
+    return render(request, 'cart_summary.html', {'cart_items': cartItems, 'total_products_card': total_products_in_card})
